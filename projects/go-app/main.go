96012e9a0b89a43896177645b226f8d6bdd670e0
Ryan Cargan <ryancargan@gmail.com>
2022-06-12 17:19:15 +0530

    Change main.go for quick test

--- projects/go-app/main.go
+++ projects/go-app/main.go
@@ -1,35 +1,19 @@
 package main
 
-// import "C"
 import (
 	"fmt"
 	"net/http"
 )
 
-func root(w http.ResponseWriter, req *http.Request) {
-
-	fmt.Fprintf(w, "Hello from the server!\n")
-}
-
-func hello(w http.ResponseWriter, req *http.Request) {
-
-	fmt.Fprintf(w, "Hello.\n")
-}
-
-func headers(w http.ResponseWriter, req *http.Request) {
-
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
-	}
-}
-
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from your API.")
+	})
 
-	http.HandleFunc("/", root)
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Path[len("/greet/"):]
+		fmt.Fprintf(w, "Hello %s\n", name)
+	})
 
 	http.ListenAndServe(":4000", nil)
 }
