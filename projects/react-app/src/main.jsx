fa7f1e94553faaae1638297149e074d803a68cf1
Ryan Cargan <ryancargan@gmail.com>
2022-06-13 02:37:15 +0530

    Add MDX styling

--- projects/react-app/src/main.jsx
+++ projects/react-app/src/main.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
+import { MDXProvider } from '@mdx-js/react'
 import './styles/main.scss'
 
 import App from './App'
 
 const e = createRoot
 
-e(document.getElementById('root')).render(
-  /*
-    Turn off strict mode to avoid double invocation of functions passed to useState, useMemo, or useReducer in dev builds (strict mode won't affect production builds).
-    
-    May have caused some bugs while using useEffect to set state (with useState) when a URL is visited directly, by detecting 'window.location.pathname'.
+const Paragraph = ({ children }) => <p className="mdx-paragraph">{children}</p>
+const Section = ({ children }) => <p className="mdx-section">{children}</p>
+const Ulist = ({ children }) => <p className="mdx-ulist">{children}</p>
 
-    TODO: See if there's a cleaner way to handle that.
-  */
+e(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <App />
+      <MDXProvider
+        components={{ 
+          p: Paragraph,
+          section: Section,
+          ul: Ulist,
+      }}>
+        <App />
+      </MDXProvider>
     </HelmetProvider>
   </React.StrictMode>,
 )
