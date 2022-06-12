f64194bef97f56ee7afcebf256a2d49c9291261b
Ryan Cargan <ryancargan@gmail.com>
2022-06-12 17:23:52 +0530

    Add Content-Type headers

--- projects/go-app/main.go
+++ projects/go-app/main.go
@@ -1,19 +1,38 @@
 package main
 
+// import "C"
 import (
 	"fmt"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from your API.")
-	})
+func root(w http.ResponseWriter, req *http.Request) {
 
-	http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
-		name := r.URL.Path[len("/greet/"):]
-		fmt.Fprintf(w, "Hello %s\n", name)
-	})
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "Hello from the server!\n")
+}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "Hello.\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+
+	http.HandleFunc("/", root)
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
 
 	http.ListenAndServe(":4000", nil)
 }
