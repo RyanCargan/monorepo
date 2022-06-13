4389114d78e47f42f0e259324eb9b7a7652cd2b6
Ryan Cargan <ryancargan@gmail.com>
2022-06-13 23:06:05 +0530

    Add basic mux for REST & WebSocket API
    Basic boilerplate ready to build with.
    Get dbutils working for user data & session store.
    Use handlers file for putting route controllers in later.

--- projects/go-app/dbutils_test.go
+++ projects/go-app/dbutils_test.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"testing"
-	"webapi/utils"
+	"webapi/dbutils"
 )
 
 func TestCreateQuery(t *testing.T) {
-	db, _ := utils.DatabaseConnection("sqlite3", "bin/sqlite.db")
-	db.MustExec(utils.Schema)
-	utils.CreateTransaction(db)
+	db, _ := dbutils.DatabaseConnection("sqlite3", "bin/sqlite.db")
+	db.MustExec(dbutils.Schema)
+	dbutils.CreateTransaction(db)
 
-	users := []utils.User{}
+	users := []dbutils.User{}
 	db.Select(&users, "SELECT * FROM user ORDER BY first_name ASC")
 
 	got := users
-	want := []utils.User{
+	want := []dbutils.User{
 		{FirstName: "Jason", LastName: "Moiron", Email: "jmoiron@jmoiron.net", Password: "supersecret"},
 		{FirstName: "John", LastName: "Doe", Email: "johndoeDNE@gmail.net", Password: "supersecret"},
 		{FirstName: "Jason", LastName: "Moiron", Email: "jmoiron@jmoiron.net", Password: "supersecret"},
--- projects/go-app/go.mod
+++ projects/go-app/go.mod
@@ -3,6 +3,8 @@ module webapi
 go 1.18
 
 require (
+	github.com/gorilla/websocket v1.5.0
+	github.com/google/uuid v1.3.0
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/mattn/go-sqlite3 v1.14.13
 )
