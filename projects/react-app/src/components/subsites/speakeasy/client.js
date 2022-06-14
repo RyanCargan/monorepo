2db36ad9fc5b345955f518c9bf54565186f475dd
Ryan Cargan <ryancargan@gmail.com>
2022-06-14 11:23:50 +0530

    Set wss route

--- projects/react-app/src/components/subsites/speakeasy/client.js
+++ projects/react-app/src/components/subsites/speakeasy/client.js
@@ -1,4 +1,8 @@
-var socket = new WebSocket("ws://localhost:4001/ws")
+/*
+  Change this to wss://codinghermit.net/socketapi/ws before any git commit/push!
+*/
+// var socket = new WebSocket("ws://localhost:4001/ws") // Only for local testing!
+var socket = new WebSocket("wss://codinghermit.net/socketapi/ws")
 
 let connect = () => {
   console.log("Attempting Connection...")
