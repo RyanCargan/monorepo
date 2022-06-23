b66213aa52fe63f87d991159c6dd1ec593a3a440
Ryan Cargan <ryancargan@gmail.com>
2022-06-23 21:18:08 +0530

    Add fade animation with react-spring

--- projects/react-app/src/components/blogs/Ryan/Articles.jsx
+++ projects/react-app/src/components/blogs/Ryan/Articles.jsx
@@ -1,50 +1,40 @@
-import React, { useState } from 'react'
-import {
-	// useSpring,
-	// animated,
-	// useSpringRef,
-	useTransition,
-} from '@react-spring/web'
+import React, { useState, useEffect } from 'react'
+import { useTransition, animated, config, easings } from '@react-spring/web'
 import { Waypoint } from 'react-waypoint'
-// import { Link, useLocation } from 'wouter'
 
 // Custom Components
 import FadeIn from '../../animation/FadeIn'
-import FadeOut from '../../animation/FadeOut'
+// import FadeOut from '../../animation/FadeOut'
 
 const Articles = () => {
-	// const [location, setLocation] = useLocation()
 	const [topic, setTopic] = useState(0)
-	// const springProps = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } })
 
-	// const handler = (n) => {
-	// 	setTopic(n)
-	// }
-
-	// React-Spring animations for conditonal rendering
-	// const [step, setStep] = useState(1)
-
-	// const transRef = useSpringRef()
+	const transition = useTransition(topic, {
+		// from: { x: -100, y: 800, opacity: 0 },
+		// enter: { x: 0, y: 0, opacity: 1},
+		// leave: { x: 100, y: 800, opacity: 0},
+		from: {opacity: 0},
+		enter: {opacity: 1},
+		leave: {opacity: 0},
+		delay: 200,
+		easing: easings.easeInOutQuart,
+    	// config: config.molasses,
+	})
 
 	// useEffect(() => {
-	// 	transRef.start();
-	// }, [step])
-
-	// const transitions = useTransition(step, {
-	// 	ref: transRef,
-	// 	keys: null,
-	// 	from: {
-	// 		...transition, styles
-	// 	},
-	// 	enter: { opacity: 1, transform: 'translateX(0px)' },
-	// 	leave: {
-	// 		...transition, styles
-	// 	},
-	// 	trail: 300, // this is delay between the leave and enter animation - enter will happen 300ms after leave in this case.
-	// 	config: { duration: 150 }
-	// })
-
-	const transition = useTransition(topic, {})
+	// 	// if (topic) {
+	// 	//   document.body.classList.add("animation")
+	// 	// } else {
+	// 	//   document.body.classList.remove("animation")
+	// 	// }
+	// 	document.body.classList.add("animation")
+	// 	setTimeout(() => {
+	// 		document.body.classList.remove("animation")
+	// 	}, 1000)
+	// }, [topic])
+	// // useEffect( () =>{
+	// // 	document.body.classList.remove("animation")
+	// // }, [] )
 
 return (
 <div style={{textAlign: 'center'}}>
@@ -61,40 +51,40 @@ return (
 	className='header-block'>Java</button>
 	<br />
 
-	{topic === 1 &&
-	<FadeOut>
-	<FadeIn>
-	{/* <animated.div style={springProps}> */}
-	<div>
-	<button className='list-block'>Java's Lineage & Evolution</button>
-	<br />
-	<button className='list-block'>SQL, JSON & NoSQL Databases</button>
-	<br />
-	<button className='list-block'>Java, SQL & the Role of JOOQ</button>
-	<br />
-	<button className='list-block'>Searching & Sorting in Java</button>
-	<br />
-	<button className='list-block'>Using Built-In Data Structures & Creating New Ones in Java</button>
-	<br />
-	<button className='list-block'>Polymorphism with Method Overloading & Overriding</button>
-	<br />
-	<button className='list-block'>Java Threads, Concurrency & Project Loom</button>
-	<br />
-	<button className='list-block'>The Evolution of Exception Handling in Java</button>
-	<br />
-	<button className='list-block'>Using Java Collections</button>
-	<br />
-	<button className='list-block'>Arrays & ArrayLists in Java</button>
-	<br />
-	<button className='list-block'>The Different Use Cases of Arrays in Programming</button>
-	<br />
-	<button className='list-block'>The Main Components of the Java Programming Language</button>
-	<br />
-	<button className='list-block'>Common Object-Oriented Concepts for Job Interviews</button>
-	{/* </animated.div> */}
+	{/* {topic === 1 && */}
+	<div className='list-container'>
+	{transition((style, item) =>
+		item === 1 ?
+		<animated.div style={style} className='list-group'>
+		<button className='list-block'>Java's Lineage & Evolution</button>
+		<br />
+		<button className='list-block'>SQL, JSON & NoSQL Databases</button>
+		<br />
+		<button className='list-block'>Java, SQL & the Role of JOOQ</button>
+		<br />
+		<button className='list-block'>Searching & Sorting in Java</button>
+		<br />
+		<button className='list-block'>Using Built-In Data Structures & Creating New Ones in Java</button>
+		<br />
+		<button className='list-block'>Polymorphism with Method Overloading & Overriding</button>
+		<br />
+		<button className='list-block'>Java Threads, Concurrency & Project Loom</button>
+		<br />
+		<button className='list-block'>The Evolution of Exception Handling in Java</button>
+		<br />
+		<button className='list-block'>Using Java Collections</button>
+		<br />
+		<button className='list-block'>Arrays & ArrayLists in Java</button>
+		<br />
+		<button className='list-block'>The Different Use Cases of Arrays in Programming</button>
+		<br />
+		<button className='list-block'>The Main Components of the Java Programming Language</button>
+		<br />
+		<button className='list-block'>Common Object-Oriented Concepts for Job Interviews</button>
+		</animated.div>
+		:''
+	)}
 	</div>
-	</FadeIn>
-	</FadeOut>}
 </div>
 )
 }
